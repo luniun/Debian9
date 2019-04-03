1af5992565c28ec452ee9fe96c0df3bc9d5f54d5
luniun <adomart@gmail.com>
2019-04-03 16:22:03 +0200

    Delete netdata.sh

--- netdata.sh
+++
@@ -1,7 +0,0 @@
-apt-get install zlib1g-dev gcc make nginx-full git autoconf autogen automake pkg-config uuid-dev zlib1g-dev  -y
-cd /opt
-git clone https://github.com/firehol/netdata.git netdata --depth=1
-cd netdata
-./netdata-installer.sh
-apt-get install apache2-utils -y
-htpasswd -c /etc/nginx/.htpasswd luniun
